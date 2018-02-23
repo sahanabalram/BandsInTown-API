57291cc30052f8a76cd9d3fd456e66846a1eb867
Sahana Balram <sahana2008balram@gmail.com>
2018-02-23 14:44:43 -0800

    Final code

--- bands.js
+++ bands.js
@@ -1,35 +1,30 @@
 function searchBandsInTown(artist) {
-    // Add code to query the bands in town api searching for the artist recieved as an argument to this function
-    let queryURL = "https://rest.bandsintown.com/artists" + artist + "?app_id=musicLove";
+    // Querying the bandsintown api for the selected artist, the ?app_id parameter is required, but can equal anything
+    let queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
     $.ajax({
-        method: GET,
-        url: queryURL
-    }).done(function(response){
-        // Console.log the entire response
+        url: queryURL,
+        method: "GET"
+    }).done(function (response) {
+        // Printing the entire object to console
         console.log(response);
+        // Constructing HTML containing the artist information
+        let artistName = $("<h1>").html(response.name);
+        let artistURL = $("<a>").attr("href", response.url).append(artistName);
+        let artistImage = $("<img>").attr("src", response.thumb_url);
+        let trackerCount = $("<h2>").html(response.tracker_count + " fans tracking this artist");
+        let upcomingEvents = $("<h2>").html(response.upcoming_event_count + " upcoming events");
+        let goToArtist = $("<a>").attr("href", response.url).text("See Tour Dates");
+        // Empty the contents of the artist-div, append the new artist content
+        $("#artist-div").empty();
+        $("#artist-div").append(artistURL, artistImage, trackerCount, upcomingEvents, goToArtist);
     });
-    // Using jQuery, append the following to the #artist-div :
-    // The artist's name
-    let artistName = $("<h3>").text(response.name);
-    // The artists thumbnail image
-    let artistImage = $("<img>").attr("src",response.thumb_url);
-    // The number of fans tracking this artist
-    let fansTracking = $("<h4>").text(response.tracker_count);
-    // The number of upcoming events for this artist
-    let upcomingEvent = $("<h5>").text(response.upcoming_event_count)
-    // A link to the bandsintown url for this artist
-    let bandsURL = $("<a>").attr("href",response.url).append(artistName);
-    let goToArtist = $("<a>").attr("href", response.url).text("See Tour Dates");
-    // Note: Append actual HTML elements, not just text
-    $("#artist-div").empty();
-    $("#artist-div").append(artistURL, artistImage, trackerCount, upcomingEvents, goToArtist);
 }
 // Event handler for user clicking the select-artist button
 $("#select-artist").on("click", function (event) {
     // Preventing the button from trying to submit the form
     event.preventDefault();
     // Storing the artist name
-    var artist = $("#artist-input").val().trim();
+    let artist = $("#artist-input").val().trim();
     // Running the searchBandsInTown function (passing in the artist as an argument)
     searchBandsInTown(artist);
 });
